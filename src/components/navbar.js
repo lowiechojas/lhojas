9cababcb3e9fe46ff6a7fefe46c0893494952270
lowiechojas <81959291+lowiechojas@users.noreply.github.com>
2025-06-05 13:22:40 +0800

    nav menu in horizontal

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
-
+import '../App.css';
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About Me</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <nav className="nav-list">
+      <ul className="nav-list-items">
+        <li className="nav-menu"><Link to="/">Home</Link></li>
+        <li className="nav-menu"><Link to="/about">About Me</Link></li>
+        <li className="nav-menu"><Link to="/projects">Projects</Link></li>
+        <li className="nav-menu"><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
