66123dc460f9e1e979de50ca7a99a18c74d91143
lowiechojas <81959291+lowiechojas@users.noreply.github.com>
2025-06-05 22:24:10 +0800

    push updates

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import '../App.css'; // Adjust the path as necessary
+import image1 from '../assets/image1.jpg'; // Adjust the path as necessary
 
 function Home() {
   return (
     <div>
       <h1>Welcome to My Portfolio</h1>
       <p>This is the landing page.</p>
+      <img src={image1} alt="Light Bulb" />
     </div>
   );
 }
